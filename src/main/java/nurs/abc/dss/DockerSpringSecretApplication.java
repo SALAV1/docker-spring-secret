ffbbfb16fc1cba4e81d101d49c34336c77563831
Nguyễn Bảo Khôi <nbkhoi@tma.com.vn>
2021-10-31 10:14:40 +0700

    init Spring Boot project

---
+++ src/main/java/nurs/abc/dss/DockerSpringSecretApplication.java
@@ -0,0 +1,13 @@
+package nurs.abc.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringSecretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringSecretApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nurs/abc/dss/DockerSpringSecretApplicationTests.java
@@ -0,0 +1,13 @@
+package nurs.abc.dss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringSecretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
